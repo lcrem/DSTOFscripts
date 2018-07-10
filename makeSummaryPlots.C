99b0e2c2a8646f740224e5168eb2c468fa0870b2
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2018-07-10 11:36:07 +0100

    Add macro to make summary plots

---
+++ makeSummaryPlots.C
@@ -0,0 +1,186 @@
+string base="/unix/dune/tof/";
+string date="2018Jul09";
+string barname="D1719";
+string POSITIONS[]={"16cm", "32cm", "48cm", "64cm", "80cm", "96cm", "112cm"};
+string THRESHOLDS[]={"5mV", "10mV", "15mV", "20mV", "25mV"};
+int colors []      ={kBlue+1, kCyan+1, kGreen+1, kOrange+1, kRed, kViolet};
+string CHANNELS[]={"Ch2", "Ch3"};
+string divisions="20mVdiv";
+
+
+void makeSummaryPlots(){
+
+  int npos = sizeof(POSITIONS)/sizeof(POSITIONS[0]);
+  int nthresh = sizeof(THRESHOLDS)/sizeof(THRESHOLDS[0]);
+  int nch = sizeof(CHANNELS)/sizeof(CHANNELS[0]);
+  
+  int ich = 1;
+
+  TGraph *gAcqTime[10];
+  TGraphErrors *gTimeDifference[10];
+  TGraphErrors *gTimeResolution[10];
+  TGraphErrors *gPEdistPMTA[10];
+  TGraphErrors *gPEdistPMTB[10];
+
+
+
+  double x[10], xerr[10], acqTime[10];
+  double timesource, timecosmics, maxtime;
+  maxtime=0;
+  double resmean[10], resmeanerr[10], resrms[10], resrmserr[10];
+  double pemeanA[10], permsA[10], pemeanB[10], permsB[10];
+  double maxpeA, maxpeB;
+  maxpeA=maxpeB=0;
+
+  string baseoutputname =   base + date + "/Bar" + barname + "/Trig" + CHANNELS[ich]  ;
+
+  TLegend *leg = new TLegend(0.7, 0.65, 0.89, 0.89, "Trigger thresholds");
+
+  for (int ithresh=0; ithresh<nthresh; ithresh++){
+   
+    for (int ipos=0; ipos<npos; ipos++){
+      
+      timesource=timecosmics=0;
+      string basename   =   base + date + "/Bar" + barname + "/SourceAt" + POSITIONS[ipos] + "/Trig" + CHANNELS[ich] + "_thres" + THRESHOLDS[ithresh] ;
+      ifstream f0 (Form("%s.txt", basename.c_str()));
+      string line;
+      getline(f0, line);
+      getline(f0, line);
+      getline(f0, line);
+      getline(f0, line);
+      getline(f0, line);
+      getline(f0, line);
+      stringstream ss(line);
+      ss >> timesource >> timecosmics;
+      
+      x[ipos] = 16*(ipos+1);
+      xerr[ipos] = 0;
+      acqTime[ipos] = timesource;
+      if (acqTime[ipos]>maxtime) maxtime=acqTime[ipos];
+
+      getline(f0, line);
+      getline(f0, line);
+      stringstream ss1(line);
+      ss1 >> resmean[ipos] >> resmeanerr[ipos] >> resrms[ipos] >> resrmserr[ipos];
+
+      getline(f0, line);
+      getline(f0, line);
+      stringstream ss2(line);
+      ss2 >> pemeanA[ipos] >> permsA[ipos] >> pemeanB[ipos] >> permsB[ipos];
+      if (pemeanA[ipos]>maxpeA) maxpeA = pemeanA[ipos];
+      if (pemeanB[ipos]>maxpeB) maxpeB = pemeanB[ipos];
+
+      f0.close();
+
+
+    }   
+
+    gAcqTime[ithresh] = new TGraph(npos, x, acqTime);
+    gAcqTime[ithresh]->SetLineColor(colors[ithresh]);
+    gAcqTime[ithresh]->SetLineWidth(2);
+    leg->AddEntry(gAcqTime[ithresh], THRESHOLDS[ithresh].c_str(), "l");
+
+    gTimeDifference[ithresh] = new TGraphErrors(npos, x, resmean, xerr, resmeanerr);
+    gTimeDifference[ithresh]->SetLineColor(colors[ithresh]);
+    gTimeDifference[ithresh]->SetLineWidth(2);
+
+    gTimeResolution[ithresh] = new TGraphErrors(npos, x, resrms, xerr, resrmserr);
+    gTimeResolution[ithresh]->SetLineColor(colors[ithresh]);
+    gTimeResolution[ithresh]->SetLineWidth(2);
+
+    gPEdistPMTA[ithresh] = new TGraphErrors(npos, x, pemeanA, xerr, permsA);
+    gPEdistPMTA[ithresh]->SetLineColor(colors[ithresh]);
+    gPEdistPMTA[ithresh]->SetLineWidth(2);
+
+    gPEdistPMTB[ithresh] = new TGraphErrors(npos, x, pemeanB, xerr, permsB);
+    gPEdistPMTB[ithresh]->SetLineColor(colors[ithresh]);
+    gPEdistPMTB[ithresh]->SetLineWidth(2);
+
+  }
+
+
+  TCanvas *cAcqTime = new TCanvas("cAcqTime");
+  cAcqTime->SetLogy();
+  
+  for (int ithresh=0; ithresh<nthresh; ithresh++){
+    if (ithresh==0){
+      gAcqTime[ithresh]->SetMaximum(maxtime*2);
+      gAcqTime[ithresh]->SetTitle(Form("Bar %s, triggering on %s;Source distance [cm];Acquisition time [s]", barname.c_str(), CHANNELS[ich].c_str()));
+      gAcqTime[ithresh]->Draw("Al");
+    } else {
+      gAcqTime[ithresh]->Draw("l");
+    }
+  }
+  leg->Draw();
+  cAcqTime->Print((baseoutputname+"_acqTime.png").c_str());
+  cAcqTime->Print((baseoutputname+"_acqTime.pdf").c_str());
+
+
+  TCanvas *cTimeDifference = new TCanvas("cTimeDifference");
+ 
+  for (int ithresh=0; ithresh<nthresh; ithresh++){
+    if (ithresh==0){
+      gTimeDifference[ithresh]->SetMinimum(-1.5e-8);
+      gTimeDifference[ithresh]->SetMaximum(+1.5e-8);
+      gTimeDifference[ithresh]->SetTitle(Form("Bar %s, triggering on %s;Source distance [cm];Time Difference [s]", barname.c_str(), CHANNELS[ich].c_str()));
+      gTimeDifference[ithresh]->Draw("Al");
+    } else {
+      gTimeDifference[ithresh]->Draw("l");
+    }
+  }
+  leg->Draw();
+  cTimeDifference->Print((baseoutputname+"_timeDifference.png").c_str());
+  cTimeDifference->Print((baseoutputname+"_timeDifference.pdf").c_str());
+
+
+  TCanvas *cTimeResolution = new TCanvas("cTimeResolution");
+ 
+  for (int ithresh=0; ithresh<nthresh; ithresh++){
+    if (ithresh==0){
+      gTimeResolution[ithresh]->SetMinimum(0);
+      gTimeResolution[ithresh]->SetMaximum(+2e-9);
+      gTimeResolution[ithresh]->SetTitle(Form("Bar %s, triggering on %s;Source distance [cm];Time Resolution [s]", barname.c_str(), CHANNELS[ich].c_str()));
+      gTimeResolution[ithresh]->Draw("Al");
+    } else {
+      gTimeResolution[ithresh]->Draw("l");
+    }
+  }
+  leg->Draw();
+  cTimeResolution->Print((baseoutputname+"_timeResolution.png").c_str());
+  cTimeResolution->Print((baseoutputname+"_timeResolution.pdf").c_str());
+
+
+  TCanvas *cPEpmtA = new TCanvas("cPEpmtA");
+ 
+  for (int ithresh=0; ithresh<nthresh; ithresh++){
+    if (ithresh==0){
+      gPEdistPMTA[ithresh]->SetMaximum(maxpeA*1.5);
+      gPEdistPMTA[ithresh]->SetTitle(Form("Bar %s, triggering on %s, PMT A;Source distance [cm];Photo-electrons", barname.c_str(), CHANNELS[ich].c_str()));
+      gPEdistPMTA[ithresh]->Draw("Al");
+    } else {
+      gPEdistPMTA[ithresh]->Draw("l");
+    }
+  }
+  leg->Draw();
+  cPEpmtA->Print((baseoutputname+"_photoelectronsPMTA.png").c_str());
+  cPEpmtA->Print((baseoutputname+"_photoelectronsPMTA.pdf").c_str());
+
+  TCanvas *cPEpmtB = new TCanvas("cPEpmtB");
+ 
+  for (int ithresh=0; ithresh<nthresh; ithresh++){
+    if (ithresh==0){
+      gPEdistPMTB[ithresh]->SetMaximum(maxpeB*1.5);
+      gPEdistPMTB[ithresh]->SetTitle(Form("Bar %s, triggering on %s, PMT B;Source distance [cm];Photo-electrons", barname.c_str(), CHANNELS[ich].c_str()));
+      gPEdistPMTB[ithresh]->Draw("Al");
+    } else {
+      gPEdistPMTB[ithresh]->Draw("l");
+    }
+  }
+  leg->Draw();
+  cPEpmtB->Print((baseoutputname+"_photoelectronsPMTB.png").c_str());
+  cPEpmtB->Print((baseoutputname+"_photoelectronsPMTB.pdf").c_str());
+
+
+
+}
+
